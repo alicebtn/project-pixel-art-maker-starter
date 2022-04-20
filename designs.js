471fae445bca3c4e6862fdc49723e18ae2426fbf
alicebtn <104085457+alicebtn@users.noreply.github.com>
2022-04-20 17:01:55 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,44 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+var sizePicker = document.getElementById("sizePicker");
+var table = document.getElementById("pixelCanvas");
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
 
 function makeGrid() {
-
-// Your code goes here!
-
+  // Size inputs
+  // Remove the old grid if there is one
+  table.innerHTML = "";
+  // Create grid
+  for (var r = 0; r < height.value; r++) {
+    let row = document.createElement("tr");
+    for (var c = 0; c < width.value; c++) {
+      let cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
 }
+
+  // Size inputs
+  var sizePicker = document.getElementById("sizePicker");
+  var height = document.getElementById("inputHeight").value;
+  var width = document.getElementById("inputWidth").value;
+
+  // Remove the old grid if there is one
+  table.chiilder().remove();
+
+  // Create grid
+  for (var r = 0; r < height ; r++) {
+        table.append("<tr></tr>");
+        for (var c = 0; c < width; c++) {
+            table.children().last().append("<td></td>");
+
+  // Create drawing function
+    table.addEventListener("click", function () {
+      this.style.background = colorPicker.value;
+        });
+        }
+    event.preventDefault();
+   sizePicker.addEventListener("submit", ()=>{
+    makeGrid(height, width);
+});
+    };
